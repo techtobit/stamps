517a0f5654ea66e49eb0c82e4bafae5bfe84f3ac
ashraf <nurthern2016@gmail.com>
2025-12-23 12:30:14 +0600

    added url

--- app/page.js
+++ app/page.js
@@ -184,9 +184,9 @@ export default function Home() {
       <footer className="w-full bg-gray-800 text-white flex items-center justify-between px-8 flex-wrap">
         <div className="flex">
           <p className="text-sm">&copy; {new Date().getFullYear()} Stamps Maker. All rights reserved </p>
-          <p className="text-sm pl-[2px]">| Developed by <a href='ashrafuddin.vercel.app'>TTB</a> </p>
+          <p className="text-sm pl-[2px]">| Developed by <a className="text-green-600 italic" href='https://ashrafuddin.vercel.app/' target="_blank">TTB</a> </p>
         </div>
-        <div className="flex">
+        <div className="flex gap-4">
           <a href="/privacy_policy.html" className="text-sm hover:underline">Privacy Policy</a>
           <a href="/terms_and_conditions.html" className="text-sm hover:underline">Terms & Conditions</a>
         </div>
